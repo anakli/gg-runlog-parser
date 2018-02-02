cdf3a64e5804fc267dbd11718f63d872088ba952
Ana Klimovic <ana.klimov@gmail.com>
2018-02-02 06:54:13 +0000

    summarize io vs. compute stats

--- summarize_io_compute.py
+++ summarize_io_compute.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import numpy as np
 import pandas as pd
 import os
@@ -10,8 +8,16 @@ filename = sys.argv[1]
 data = pd.read_csv(filename, sep="\t", header=None)
 data.columns = [ "worker", "start-time", "disk-write", "copy-executables", "fetch-deps", "execute", "check-outfile", "upload-outfile", "end-time" ]
 
-print "upload times:\n" , data.loc[:,"upload-outfile"] 
+#print "upload times:\n" , data.loc[:,"upload-outfile"] 
 #print "fetch times:\n" , data.loc[:,"fetch-deps"] 
-print "Total fetch time:" , data["fetch-deps"].sum()
-print "Total exec time:" , data["execute"].sum()
-print "Total upload time:" , data["upload-outfile"].sum() + data["check-outfile"].sum()
+fetch = data["fetch-deps"].sum()
+compute = data["execute"].sum()
+upload = data["upload-outfile"].sum() + data["check-outfile"].sum()
+total = fetch + compute + upload
+print "Total fetch time:" , fetch, " (", fetch/total * 100, "%)"
+print "Total exec time:" , compute, " (", compute/total * 100, "%)"
+print "Total upload time:" , upload, " (", upload/total * 100, "%)"
+print "Total lambda time:", total
+print "IO time: ", (fetch+upload)/total * 100, "%, Compute time: ", compute/total * 100, "%"
+
+print "Total job runtime:" , data["end-time"].iloc[-1]
